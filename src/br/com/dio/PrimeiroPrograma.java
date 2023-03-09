ed963e0af827d65ee1c1a9299e3087efdc416d9b
heliodesouza <heliodesouza088@gmail.com>
2023-03-09 19:46:29 -0300

    arquivos teste

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+	}
+
+}
